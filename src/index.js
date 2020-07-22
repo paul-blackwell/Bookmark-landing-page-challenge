d9c02cc51f382fb5a452060a4dd4c56c3cf4a0c1
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-22 22:10:41 +0100

    File setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 import { run } from './app/app';
 import "./sass/main.scss";
-import { AlertService } from './app/alert.service';
-import { ComponentService } from './app/component.service';
-
-const alertService = new AlertService();
-const componentService = new ComponentService();
-
-run(alertService, componentService)
